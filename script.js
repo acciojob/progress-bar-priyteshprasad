78ae6a242df994193bccf7ac8bf6bddacc0e2611
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-03-07 18:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let progress = 1;
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+next.addEventListener('click', ()=>{
+	if(progress <5){
+		progress+=1;
+		let currProgress = document.getElementById(progress);
+		currProgress.classList.add("active")
+		if(progress == 5){
+			next.setAttribute("disabled", "")
+			prev.removeAttribute("disabled")
+		}
+	}
+})
+prev.addEventListener('click', ()=>{
+	if(progress >0){
+		let currProgress = document.getElementById(progress);
+		currProgress.classList.remove("active")
+		progress-=1;
+		if(progress == 0){
+			prev.setAttribute("disabled", "")
+			next.removeAttribute("disabled")
+		}
+	}
+})

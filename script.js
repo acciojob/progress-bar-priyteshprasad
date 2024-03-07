b40eb78da0822e5c54811c5c0bd24f1a319275d4
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-03-07 18:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ const next = document.getElementById("next")
 next.addEventListener('click', ()=>{
 	if(progress <5){
 		progress+=1;
-		let currProgress = document.getElementById(progress);
+		let currProgress = document.getElementById("circle-"+progress);
 		currProgress.classList.add("active")
 		if(progress == 5){
 			next.setAttribute("disabled", "")
-			prev.removeAttribute("disabled")
 		}
+			prev.removeAttribute("disabled")
 	}
 })
 prev.addEventListener('click', ()=>{
-	if(progress >0){
-		let currProgress = document.getElementById(progress);
+	if(progress >=0){
+		let currProgress = document.getElementById("circle-"+progress);
 		currProgress.classList.remove("active")
 		progress-=1;
-		if(progress == 0){
+		if(progress == 1){
 			prev.setAttribute("disabled", "")
-			next.removeAttribute("disabled")
 		}
+			next.removeAttribute("disabled")
 	}
 })
